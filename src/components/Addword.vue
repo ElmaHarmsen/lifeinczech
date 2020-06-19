5a6e5496aa4f1ceb5f01c87bb3cdce17a9836eb4
Elma Harmsen <elmaharmsen01@gmail.com>
2020-06-19 21:36:13 +0200

    Tried stuff out with form

--- src/components/Addword.vue
+++ src/components/Addword.vue
@@ -8,19 +8,24 @@
       <div class="addword_form">
         <h1>This is the form to add a word</h1>
         <div class="addword_inputs">
-          <input class="inputs_field" placeholder="Czech" type="text" name="input_word">
-          <input class="inputs_field" placeholder="English" type="text" name="input_translation">
-          <input class="inputs_field" placeholder="Pronunciation" type="text" name="input_pronunciation">
+          <input v-model="input_word" class="inputs_field" placeholder="Czech" type="text" name="input_word">
+          <input v-model="input_translation" class="inputs_field" placeholder="English" type="text" name="input_translation">
+          <input v-model="input_pronunciation" class="inputs_field" placeholder="Pronunciation" type="text" name="input_pronunciation">
           <div>
-            <input class="inputs_radio" type="radio" id="hotlist" value="0">
+            <input class="inputs_radio" type="radio" id="hotlist" v-model="addTo" value="Hotlist">
             <label for="hotlist">@ Hotlist</label>
           </div>
           <div class="dictionary">
-            <input class="inputs_radio" type="radio" id="dictionary" value="1">
+            <input class="inputs_radio" type="radio" id="dictionary" v-model="addTo" value="Dictionary">
             <label for="dictionary">@ Dictionary</label>
           </div>
-          <input class="inputs_button" type="submit" value="Add Word">
+          <input v-on:click="submit()" class="inputs_button" type="submit" name="form_submit" value="Add this word to`${addTo}`">
         </div>
+      </div>
+      <div class="addword_pre-inputs">
+        <h3>{{ input_word }}</h3>
+        <h2>{{ input_translation }}</h2>
+        <h3>{{ input_pronunciation }}</h3>
       </div>
     </div>
   </section>
@@ -32,30 +37,38 @@ export default {
   data: function() {
     return {
       wordformopen: false,
-      formDataJson: []
+      formDataJson: [],
+      input_word: "",
+      input_translation: "",
+      input_pronunciation: "",
+      form_submit: "",
+      addTo: ""
     }
   },
   methods: {
     openWordForm() {
       this.wordformopen = !this.wordformopen;
     },
-    // async fetchData() {
-    //   const addword = await fetch(
-    //     "https://dictionary--api.herokuapp.com/api/dictionarycz",
-    //     {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json" //Just how it is ;)
-    //       },
-    //       body: JSON.stringify({
-    //         input_word,
-    //         input_translation,
-    //         input_pronunciation
-    //       })
-    //     }
-    //   );
-    //   this.formDataJson = await addword.json();
-    // }
+    submit() {
+      this.$refs.form.submit()
+    },
+    async fetchData() {
+      const addword = await fetch(
+        "https://dictionary--api.herokuapp.com/api/dictionarycz",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json" //Just how it is ;)
+          },
+          body: JSON.stringify({
+            // input_word,
+            // input_translation,
+            // input_pronunciation
+          })
+        }
+      );
+      this.formDataJson = await addword.json();
+    }
   }
 }
 </script>
@@ -135,7 +148,7 @@ section {
         grid-template-columns: 1fr 1fr;
         grid-template-rows: repeat(5, 100%);
         gap: 1.5rem;
-        margin-top: 50px;
+        margin-top: 1.5rem;
 
         input {
           grid-column: 1/3;
@@ -171,6 +184,22 @@ section {
           background-color: blue;
         }
       }
+    }
+    .addword_pre-inputs {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-template-rows: 100%;
+      gap: 0.5rem;
+      padding: 0.5rem;
+
+      h2, h3 {
+        border: 1px solid blue;
+        height: auto;
+        min-height: calc(50px - 1.6rem);
+      }
+    }
+    .addword_pre-inputs > * {
+      padding: 0.8rem 0.5rem;
     }
   }
 }
